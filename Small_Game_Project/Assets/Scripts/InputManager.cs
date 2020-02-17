f7ae07733a98b07176a725cb37bff07492af15b3
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-17 01:34:25 +0000

    Started Player Input

---
+++ Small_Game_Project/Assets/Scripts/InputManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public KeyCode moveUpKey = KeyCode.W;
+    public KeyCode moveDownKey = KeyCode.S;
+    public KeyCode moveLeftKey = KeyCode.A;
+    public KeyCode moveRightKey = KeyCode.D;
+    //
+    public KeyCode actionKeyOneKey = KeyCode.J;
+    public KeyCode actionKeyTwoKey = KeyCode.K;
+    public KeyCode actionKeyThreeKey = KeyCode.L;
+    public KeyCode actionKeyFourKey = KeyCode.Semicolon;
+    //
+    //public KeyCode cameraRotHorizontal        Mouse camera input
+    //public KeyCode cameraRotVertical          Mouse camera input
+    //
+    public KeyCode menuOneKye = KeyCode.Tab;
+    public KeyCode menuTwoKye = KeyCode.Escape;
+    //
+    //
+    public bool moveUp = false;
+    public bool moveDown = false;
+    public bool moveLeft = false;
+    public bool moveRight = false;
+    //
+    public bool actionKeyOneDown = false;
+    public bool actionKeyTwoDown = false;
+    public bool actionKeyThreeDown = false;
+    public bool actionKeyFourDown = false;
+    //
+    //public bool cameraRotHorizontal
+    //public bool cameraRotVertical
+    //
+    public bool menuOneDown = false;
+    public bool menuTwoDown = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInputKeys();
+    }
+
+    void UpdateInputKeys()
+    {
+
+        moveUp = Input.GetKey(moveUpKey);
+        moveDown = Input.GetKey(moveDownKey);
+        moveLeft = Input.GetKey(moveLeftKey);
+        moveRight = Input.GetKey(moveRightKey);
+        //
+        actionKeyOneDown = Input.GetKeyDown(actionKeyOneKey);
+        actionKeyTwoDown = Input.GetKeyDown(actionKeyTwoKey);
+        actionKeyThreeDown = Input.GetKeyDown(actionKeyThreeKey);
+        actionKeyFourDown = Input.GetKeyDown(actionKeyFourKey);
+        //
+        //cameraRotHorizontal
+        //cameraRotVertical
+        //
+        menuOneDown = Input.GetKeyDown(menuOneKye);
+        menuTwoDown = Input.GetKeyDown(menuTwoKye);
+
+    }
+
+}
