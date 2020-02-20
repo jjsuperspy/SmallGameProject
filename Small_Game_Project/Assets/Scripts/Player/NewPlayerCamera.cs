073868db4388e2386f2fb61a48f23feb6f84ae97
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-20 17:17:46 +0000

    Redone player movement and camera

--- Small_Game_Project/Assets/Scripts/Player/NewPlayerCamera.cs
+++ Small_Game_Project/Assets/Scripts/Player/NewPlayerCamera.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class NewPlayerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float cameraSpeed = 100f;
+    public float xRotation = 0f;
+    public Transform player;
+
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float cameraX = Input.GetAxis("CameraMovementX") * cameraSpeed * Time.deltaTime;
+        float cameraY = Input.GetAxis("CameraMovementY") * cameraSpeed * Time.deltaTime;
+
+        xRotation -= cameraY;
+        xRotation = Mathf.Clamp(xRotation, -15f, 60f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * cameraX);
     }
 }
