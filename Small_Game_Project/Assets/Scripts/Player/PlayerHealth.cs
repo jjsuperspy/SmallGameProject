dbb5bc200e062d019855f7d9ad536bf957d79ab8
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-21 01:50:10 +0000

    Player Health
    Health UI and a simple set up for health.

---
+++ Small_Game_Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 10;
+    public int currenthealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currenthealth -= damage;
+
+        healthBar.SetHealth(currenthealth);
+    }
+}
