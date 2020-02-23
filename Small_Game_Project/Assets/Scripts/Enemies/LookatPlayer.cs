33091b85b6575ee9e2affa93440b387047e2dba7
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-23 23:26:54 +0000

    Added First Enemy art and HDPR
    Added art for an enemy as well as re-installing HDPR and redoing the mist material.

---
+++ Small_Game_Project/Assets/Scripts/Enemies/LookatPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookatPlayer : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*if (player != null)
+        {
+            Vector3 playerPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+            transform.LookAt(playerPosition);
+        }*/
+
+        Vector3 rot = Quaternion.LookRotation(player.position - transform.position).eulerAngles;
+        rot.x = rot.z = 0;
+        transform.rotation = Quaternion.Euler(rot);
+
+    } 
+}
