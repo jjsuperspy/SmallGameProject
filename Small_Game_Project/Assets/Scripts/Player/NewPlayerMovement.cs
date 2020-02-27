872600fe662960a93ed90a87f15df9040f25edac
jjsuperspy <jj.superspy@hotmail.co.uk>
2020-02-27 21:11:59 +0000

    Weapon Toggle working

--- Small_Game_Project/Assets/Scripts/Player/NewPlayerMovement.cs
+++ Small_Game_Project/Assets/Scripts/Player/NewPlayerMovement.cs
@@ -18,16 +18,32 @@ public class NewPlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+    bool ToggleWeapon = false;
+
+    void Start()
+    {
+        ToggleWeapon = false;
+    }
     // Update is called once per frame
     void Update()
     {
+        Grounded();
+        PlayerMovement();
+        Jump();
+        WeaponToggle();
+    }
+
+    void Grounded()
+    {
         isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-
+    }
+    void PlayerMovement()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -36,7 +52,9 @@ public class NewPlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
         animator.SetFloat("Speed", speed);
-
+    }
+    void Jump()
+    {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -46,4 +64,13 @@ public class NewPlayerMovement : MonoBehaviour
 
         controller.Move(velocity * Time.deltaTime);
     }
+    void WeaponToggle()
+    {
+        if (Input.GetAxis("Weapon Toggle") !=0)
+        {
+            ToggleWeapon = !ToggleWeapon;
+            animator.SetBool("Toggle_Weapon", ToggleWeapon);
+        }
+    }
+    
 }
